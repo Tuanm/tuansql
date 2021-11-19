27b4f05a674a6caf64a49349054cf1756119b769
vi <tuanm.dev@gmail.com>
2021-11-19 13:17:58 +0700

    change main.py

--- main.py
+++ main.py
@@ -1,22 +1,28 @@
 from tuanmysql import TuanMySQL
 import mysql.connector
 
-tuan = TuanMySQL(mysql.connector.connect(
-    user='root',
-    password='',
-    database='test'
-))
+if __name__ == '__main__':
 
-result = tuan\
-    .select('name', 'age')\
-    .from_table('user')\
-    .where('age > 19', 'name = "Tuan"')\
-    .execute().first()
+    # Connect to MySQL
 
-print(result)
+    tuan = TuanMySQL(mysql.connector.connect(
+        user='root',
+        password='',
+        database='test'
+    ))
 
-tuan\
-    .update('user')\
-    .set(['name', '"Tuan"'], ['age', 12])\
-    .where('age > 17')\
-    .execute()
+    # For testing
+
+    result = tuan\
+        .select('name', 'age')\
+        .from_table('user')\
+        .where('age > 19', 'name = "Tuan"')\
+        .execute().first()
+    print(result)
+
+    tuan\
+        .update('user')\
+        .set(['name', '"Tuan"'], ['age', 12])\
+        .where('age > 17')\
+        .execute()
+
